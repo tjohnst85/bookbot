48d1538bc32790b1c46911577f6ee020e88cd6f0
trev <tjohnstone@designfusion.com>
2025-11-06 22:11:42 -0500

    I: done for now, will come back and refactor and clean up

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
+import sys
 from stats import get_num_words, get_letter_count, chars_dict_to_sorted_list
 
 
 
 def main():
-    book_path = "books/frankenstein.txt"
-    text = get_book_text(book_path)
-    text3 = get_num_words(text)
-    text2 = get_letter_count(text)
-    text4 = chars_dict_to_sorted_list(text2)
-    print_report(book_path, text3, text4)
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        book_path = sys.argv[1]
+        text = get_book_text(book_path)
+        text3 = get_num_words(text)
+        text2 = get_letter_count(text)
+        text4 = chars_dict_to_sorted_list(text2)
+        print_report(book_path, text3, text4)
     
     
 
