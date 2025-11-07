8b0230acb93dc02655d7ed1419f714d543cc82c8
trev <tjohnstone@designfusion.com>
2025-11-06 21:47:40 -0500

    G: removing pycache using gitignore

--- stats.py
+++ stats.py
@@ -32,3 +32,7 @@ def chars_dict_to_sorted_list(num_chars_dict):
         sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
     sorted_list.sort(reverse=True, key=sort_on)
     return sorted_list
+
+
+
+
