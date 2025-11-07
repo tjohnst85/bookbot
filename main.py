056f50f389b822624b1e2cc5ebf779c2998931e2
trev <tjohnstone@designfusion.com>
2025-11-06 20:55:04 -0500

    E: now have letter count

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from stats import get_num_words
+from stats import get_letter_count
 
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    print(get_num_words(text))
+    print(get_letter_count(text))
     
 
 def get_book_text(book):
--- stats.py
+++ stats.py
@@ -4,3 +4,20 @@ def get_num_words(text):
     print(f"Found {word_count} total words")
 
 
+def get_letter_count(text):
+    unique_letter_set = set()
+    unique_letter_counter = {}
+    textlower = text.lower()
+    letters = list(textlower)
+    for l in letters:
+        unique_letter_set.add(l)
+
+    unique_letter_count = {item:0 for item in unique_letter_set}
+
+    for l in letters:
+        if l in unique_letter_count:
+            unique_letter_count[l] += 1
+
+
+
+
