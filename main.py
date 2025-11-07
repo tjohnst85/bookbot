4810b52b305f3576f32163bb4b6fc7fc371bd4e9
trev <tjohnstone@designfusion.com>
2025-11-06 20:02:50 -0500

    D: refactored to two files

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+from stats import get_num_words
+
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    word_list = text.split()
-    word_count = len(word_list)
-    print(f"Found {word_count} total words")
+    print(get_num_words(text))
+    
 
 def get_book_text(book):
     with open(book) as f:
---
+++ stats.py
@@ -0,0 +1,6 @@
+def get_num_words(text):
+    word_list = text.split()
+    word_count = len(word_list)
+    print(f"Found {word_count} total words")
+
+
