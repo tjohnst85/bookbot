87240d517e17778c2670a37705c618e8c0e7283f
trev <tjohnstone@designfusion.com>
2025-11-06 21:43:06 -0500

    F: Reprot added - this is currently a mess. practice clean up and simlificartions next

--- main.py
+++ main.py
@@ -1,14 +1,32 @@
-from stats import get_num_words
-from stats import get_letter_count
+from stats import get_num_words, get_letter_count, chars_dict_to_sorted_list
+
+
 
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    print(get_letter_count(text))
+    text3 = get_num_words(text)
+    text2 = get_letter_count(text)
+    text4 = chars_dict_to_sorted_list(text2)
+    print_report(book_path, text3, text4)
+    
     
 
 def get_book_text(book):
     with open(book) as f:
         return f.read()
+
+def print_report(book_path, num_words, chars_sorted_list):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"{item['char']}: {item['num']}")
+
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -1,13 +1,14 @@
 def get_num_words(text):
     word_list = text.split()
     word_count = len(word_list)
-    print(f"Found {word_count} total words")
+    return word_count
 
 
 def get_letter_count(text):
+    textlower = text.lower()
     unique_letter_set = set()
     unique_letter_counter = {}
-    textlower = text.lower()
+            
     letters = list(textlower)
     for l in letters:
         unique_letter_set.add(l)
@@ -18,6 +19,16 @@ def get_letter_count(text):
         if l in unique_letter_count:
             unique_letter_count[l] += 1
 
+    return unique_letter_count
 
 
+def sort_on(d):
+    return d["num"]
 
+
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
